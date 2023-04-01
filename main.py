b67f55591d531b494c408b9717df1e698ecbd0bb
muhdrayan <muhammedrayansavad1@gmail.com>
2023-04-01 23:28:43 +0300

    moves file into "downloads" folder

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 # MAIN FILE
 
 from pytube import YouTube
+from datetime import datetime
+import os
+
 
 def download_video(url):
-    yt = YouTube(url)
 
+    start = datetime.now()
+
+    yt = YouTube(url)
     yt = yt.streams.get_highest_resolution()
 
     try:
@@ -12,6 +17,11 @@ def download_video(url):
     except:
         return False
     
-    return True
+    files = os.listdir()
+    for file in files:
+        if file.endswith(".mp4"):
+            os.replace(file, f"./downloads/{file}")
+
+    return (datetime.now() - start).seconds
 
 print(download_video("https://www.youtube.com/watch?v=JIPQbgPk4oA"))
