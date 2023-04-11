00f1200d930ec56c854e92f983e98692ae98da07
muhdrayan <muhammedrayansavad1@gmail.com>
2023-04-11 23:15:22 +0300

    added 3 new commands

--- locker.py
+++ locker.py
@@ -1,18 +1,19 @@
+from AppOpener import open as open_app
+from AppOpener import close as close_app
 from pynput import keyboard
-from tkinter import messagebox
 import json
 
-COMBINATIONS = [{keyboard.Key.shift_r, keyboard.Key.f12}]
+COMBINATIONS = [{keyboard.Key.shift_r, keyboard.Key.f11}, {keyboard.Key.shift_r}, {keyboard.Key.shift_r, keyboard.Key.f12}, {keyboard.Key.shift_r, keyboard.Key.f10}]
 FILE_PATH = "E:/Programming/Python/Assistant/Assistant/data/apps/locker/config.json"
 
 current = set()
 
-def change_perms():
+def change_perms(mode=None):
         
     with open(FILE_PATH) as f:
         config = json.load(f)
 
-    config["lock"] = not config["lock"]
+    config["lock"] = not config["lock"] if mode is None else mode
     with open(FILE_PATH, mode='w') as f:
         json.dump(config, f, indent=4)
 
@@ -26,8 +27,26 @@ def on_press(key):
         if any(all(k in current for k in combo) for combo in COMBINATIONS):
             change = change_perms()
             print(change)
-            messagebox.showinfo("Changed Lock", f"Mode set to {'un' if change else ''}locked!")
 
+            if current == COMBINATIONS[0]:
+                change_perms(True)
+                try:
+                    close_app("discord")
+                except:
+                    print("Couldn't close")
+            elif current == COMBINATIONS[2]:
+
+                change_perms(False)
+                try:
+                    open_app("discord")
+                except:
+                    print("Couldn't open")
+
+        elif current == COMBINATIONS[3]:
+            exit()
+
+
+                
 
 def on_release(key):
     if any([key in combo for combo in COMBINATIONS]):
